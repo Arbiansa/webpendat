6e1f0c6481c9568e6f9a8b361a29dac803156109
Arbiansa <111883775+Arbiansa@users.noreply.github.com>
2022-12-01 06:00:57 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,7 +1,14 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import random as rnd
 
 import streamlit as st
+import pip
+pip.main(['install','seaborn'])
+import matplotlib.pyplot as plt
+
+# visualization
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
@@ -9,8 +16,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-st.title("PENAMBANGAN DATA")
-st.write("By: Indyra Januar - 200411100022")
+# data analysis 
+import pandas as pd
+import numpy as np
+import random as rnd
+
+import warnings
+warnings.filterwarnings("ignore")
+
+st.title("Web Data Mining")
+st.write("By: Muhammad Arbiansyafa Siswanto - 200411100195")
 st.write("Grade: Penambangan Data C")
 upload_data, preporcessing, modeling, implementation = st.tabs(
     ["Upload Data", "Prepocessing", "Modeling", "Implementation"])
@@ -18,8 +33,8 @@ upload_data, preporcessing, modeling, implementation = st.tabs(
 
 with upload_data:
     st.write("""# Upload File""")
-    st.write("Dataset yang digunakan pada percobaan ini adalah data penyakit jantung yang di dapat dari UCI (Univercity of California Irvine)")
-    st.write("link dataset : https://archive.ics.uci.edu/ml/datasets/Heart+Disease")
+    st.write("Dataset yang digunakan pada percobaan ini adalah data Data Churn Telekomunikasi (Membersihkan Dataset Churn Pelanggan Orange Telecom)")
+    st.write("link dataset :https://www.kaggle.com/datasets/mnassrib/telecom-churn-datasets?resource=download")
     st.write("Terdiri dari 270 dataset terdapat 13 atribut dan 2 kelas.")
     st.write("Heart Attack (Serangan Jantung) adalah kondisi medis darurat ketika darah yang menuju ke jantung terhambat.")
 
@@ -32,60 +47,79 @@ with upload_data:
 
 with preporcessing:
     st.write("""# Preprocessing""")
-   
+
     df_data_train = df_train.copy()
-    df_data_test=df_train.copy()
-    
+    df_data_test = df_train.copy()
+
     df_data_train.tail()
-    
-    df_data_train['International plan'].replace(['No','Yes'],[0,1],inplace=True)
-    df_data_test['International plan'].replace(['No','Yes'],[0,1],inplace=True)
+
+    df_data_train['International plan'].replace(
+        ['No', 'Yes'], [0, 1], inplace=True)
+    df_data_test['International plan'].replace(
+        ['No', 'Yes'], [0, 1], inplace=True)
     df_data_train['International plan'].value_counts()
 
-    df_data_train['Voice mail plan'].replace(['No','Yes'],[0,1],inplace=True)
-    df_data_test['Voice mail plan'].replace(['No','Yes'],[0,1],inplace=True)
+    df_data_train['Voice mail plan'].replace(
+        ['No', 'Yes'], [0, 1], inplace=True)
+    df_data_test['Voice mail plan'].replace(
+        ['No', 'Yes'], [0, 1], inplace=True)
     df_data_train['Voice mail plan'].value_counts()
-    
+
     st.write(df_data_train['Voice mail plan'].isnull().sum())
     st.write(df_data_train['International plan'].isnull().sum())
-    
+
     st.write(df_data_train.head())
-        
-    df_data_train['Total charge amount'] = df_data_train['Total day charge'] + df_data_train['Total eve charge'] + df_data_train['Total night charge']+ df_data_train['Total intl charge']
-    df_data_train['Total call minutes'] = df_data_train['Total day minutes'] + df_data_train['Total eve minutes'] + df_data_train['Total night minutes'] + df_data_train['Total intl minutes']
-    df_data_train['Total number of calls'] = df_data_train['Total day calls'] + df_data_train['Total eve calls'] + df_data_train['Total night calls'] + df_data_train['Total intl calls'] 
-    
+
+    df_data_train['Total charge amount'] = df_data_train['Total day charge'] + \
+        df_data_train['Total eve charge'] + \
+        df_data_train['Total night charge'] + \
+        df_data_train['Total intl charge']
+    df_data_train['Total call minutes'] = df_data_train['Total day minutes'] + \
+        df_data_train['Total eve minutes'] + \
+        df_data_train['Total night minutes'] + \
+        df_data_train['Total intl minutes']
+    df_data_train['Total number of calls'] = df_data_train['Total day calls'] + \
+        df_data_train['Total eve calls'] + \
+        df_data_train['Total night calls'] + df_data_train['Total intl calls']
+
     st.write(df_data_train.sample(10))
-    
-    df_data_test['Total charge amount'] = df_data_test['Total day charge'] + df_data_test['Total eve charge'] + df_data_test['Total night charge']+ df_data_test['Total intl charge']
-    df_data_test['Total call minutes'] = df_data_test['Total day minutes'] + df_data_test['Total eve minutes'] + df_data_test['Total night minutes'] + df_data_test['Total intl minutes']
-    df_data_test['Total number of calls'] = df_data_test['Total day calls'] +df_data_test['Total eve calls'] + df_data_test['Total night calls'] + df_data_test['Total intl calls'] 
+
+    df_data_test['Total charge amount'] = df_data_test['Total day charge'] + \
+        df_data_test['Total eve charge'] + \
+        df_data_test['Total night charge'] + df_data_test['Total intl charge']
+    df_data_test['Total call minutes'] = df_data_test['Total day minutes'] + \
+        df_data_test['Total eve minutes'] + \
+        df_data_test['Total night minutes'] + \
+        df_data_test['Total intl minutes']
+    df_data_test['Total number of calls'] = df_data_test['Total day calls'] + \
+        df_data_test['Total eve calls'] + \
+        df_data_test['Total night calls'] + df_data_test['Total intl calls']
 
     st.write(df_data_test.sample(10))
-    
+
 with modeling:
     df_data_train = df_train.copy()
-    df_data_test=df_train.copy()
+    df_data_test = df_train.copy()
 
     X_train = df_data_train.drop("Churn", axis=1)
     Y_train = df_data_train["Churn"]
-    X_test  = df_data_test.drop("Churn", axis=1)
+    X_test = df_data_test.drop("Churn", axis=1)
     Y_test = df_data_test["Churn"]
     X_train.shape, Y_train.shape, X_test.shape
-    
-    knn = KNeighborsClassifier(n_neighbors = 3)
+
+    knn = KNeighborsClassifier(n_neighbors=3)
     knn.fit(X_train, Y_train)
     Y_pred = knn.predict(X_test)
 
-    #Calculating Details
+    # Calculating Details
     acc_knn_train = round(knn.score(X_train, Y_train) * 100, 2)
-    acc_knn_test =round(knn.score(X_test, Y_test) * 100, 2)
-    print('KNN Train Score is : ',  acc_knn_train )
-    print('KNN test Score is : ' , acc_knn_test )
-    #Calculating Prediction
-    accuracy= accuracy_score(Y_test , Y_pred )
+    acc_knn_test = round(knn.score(X_test, Y_test) * 100, 2)
+    print('KNN Train Score is : ',  acc_knn_train)
+    print('KNN test Score is : ', acc_knn_test)
+    # Calculating Prediction
+    accuracy = accuracy_score(Y_test, Y_pred)
 
-    print('Accuracy Score is  = ', accuracy )
+    print('Accuracy Score is  = ', accuracy)
 
-    conf = confusion_matrix(Y_test , Y_pred )
+    conf = confusion_matrix(Y_test, Y_pred)
     print('confusion matrix \n',  conf)
