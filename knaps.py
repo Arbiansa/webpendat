9be56eb15b4327951d769236914ede7b02b607bb
Arbiansa <111883775+Arbiansa@users.noreply.github.com>
2022-12-01 06:08:16 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,27 +1,25 @@
+import warnings
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import random as rnd
 
 import streamlit as st
 import pip
-pip.main(['install','seaborn'])
-import matplotlib.pyplot as plt
+pip.main(['install', 'seaborn'])
 
 # visualization
-import matplotlib.pyplot as plt
 
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
 
-# data analysis 
-import pandas as pd
-import numpy as np
-import random as rnd
+# data analysis
 
-import warnings
 warnings.filterwarnings("ignore")
 
 st.title("Web Data Mining")
@@ -123,3 +121,6 @@ with modeling:
 
     conf = confusion_matrix(Y_test, Y_pred)
     print('confusion matrix \n',  conf)
+
+    sns.heatmap(conf, center=True)
+    plt.show()
